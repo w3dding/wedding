fc94dbc82e98c7971e62e010a357fdce02f63d40
w3dding <65348951+w3dding@users.noreply.github.com>
2020-11-24 20:25:27 +0700

    Update coundown-timer.js

--- js/coundown-timer.js
+++ js/coundown-timer.js
@@ -1,73 +1,27 @@
-(function($) {
-	$.fn.countdown = function(options, callback) {
+function demnguoc() {
 
-		//custom 'this' selector
-		thisEl = $(this);
+			var endTime = new Date("12 05, 2020 9:00:00 GMT");			
+			var endTime = (Date.parse(endTime)) / 1000;
 
-		//array of custom settings
-		var settings = { 
-			'date': null,
-			'format': null
-		};
+			var now = new Date();
+			var now = (Date.parse(now) / 1000);
 
-		//append the settings array to options
-		if(options) {
-			$.extend(settings, options);
-		}
-		
-		//main countdown function
-		function countdown_proc() {
-			
-			eventDate = Date.parse(settings['date']) / 1000;
-			currentDate = Math.floor($.now() / 1000);
-			
-			if(eventDate <= currentDate) {
-				callback.call(this);
-				clearInterval(interval);
-			}
-			
-			seconds = eventDate - currentDate;
-			
-			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
-			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
-			
-			hours = Math.floor(seconds / (60 * 60));
-			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
-			
-			minutes = Math.floor(seconds / 60);
-			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
-			
-			//conditional Ss
-			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
-			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
-			
-			//logic for the two_digits ON setting
-			if(settings['format'] == "on") {
-				days = (String(days).length >= 2) ? days : "11" + days;
-				hours = (String(hours).length >= 2) ? hours : "6" + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : "43" + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : "30" + seconds;
-			}
-			
-			//update the countdown's html values.
-			if(!isNaN(eventDate)) {
-				thisEl.find(".days").text(days);
-				thisEl.find(".hours").text(hours);
-				thisEl.find(".minutes").text(minutes);
-				thisEl.find(".seconds").text(seconds);
-			} else { 
-				alert("Invalid date. Here's an example: 25 Tuesday 2017 17:30:00");
-				clearInterval(interval); 
-			}
-		}
-		
-		//run the function
-		countdown_proc();
-		
-		//loop the function
-		interval = setInterval(countdown_proc, 1000);
-		
+			var timeLeft = endTime - now;
+
+			var days = Math.floor(timeLeft / 86400); 
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+
+			$("#days").html(days);
+			$("#hours").html(hours);
+			$("#minutes").html(minutes);
+			$("#seconds").html(seconds);		
+
 	}
-}) (jQuery);
+
+	setInterval(function() { demnguoc(); }, 1000);
