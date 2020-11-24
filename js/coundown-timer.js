40412a8dd6b48bb411713bb2b5f47244ddb40207
w3dding <65348951+w3dding@users.noreply.github.com>
2020-11-24 17:28:52 +0700

    Update coundown-timer.js

--- js/coundown-timer.js
+++ js/coundown-timer.js
@@ -45,10 +45,10 @@
 			
 			//logic for the two_digits ON setting
 			if(settings['format'] == "on") {
-				days = (String(days).length >= 2) ? days : "0" + days;
-				hours = (String(hours).length >= 2) ? hours : "0" + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
+				days = (String(days).length >= 2) ? days : "11" + days;
+				hours = (String(hours).length >= 2) ? hours : "6" + hours;
+				minutes = (String(minutes).length >= 2) ? minutes : "43" + minutes;
+				seconds = (String(seconds).length >= 2) ? seconds : "30" + seconds;
 			}
 			
 			//update the countdown's html values.
